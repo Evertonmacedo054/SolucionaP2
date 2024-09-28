bb0fc436b396b0234e55de560230849ee397c5b1
Evertonmacedo054 <evertonmacedo541@gmail.com>
2024-09-28 11:11:40 -0300

    Apenas algumas explicações

--- app/views.py
+++ app/views.py
@@ -1,7 +1,10 @@
 from app import app
-from flask import render_template
+from flask import render_template,url_for
 
 @app.route('/')
 def index():
     return render_template('index.html')
- 
+
+@app.route('/teste/')
+def teste():
+    return "teste!"
